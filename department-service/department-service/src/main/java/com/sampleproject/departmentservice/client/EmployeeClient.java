f4f71796f2d6bb412ed61d66cc0f39535297f4cd
ajilvarghese <avarghese@advalent.com>
2023-06-17 13:38:20 +0530

    new code

---
+++ department-service/department-service/src/main/java/com/sampleproject/departmentservice/client/EmployeeClient.java
@@ -0,0 +1,15 @@
+package com.sampleproject.departmentservice.client;
+
+import com.sampleproject.departmentservice.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.List;
+
+@HttpExchange
+public interface EmployeeClient {
+    @GetExchange("/employee/department/{departmentId}")
+    public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
+}
---
+++ department-service/department-service/src/main/java/com/sampleproject/departmentservice/config/WebClientConfig.java
@@ -0,0 +1,43 @@
+package com.sampleproject.departmentservice.config;
+
+import com.sampleproject.departmentservice.client.EmployeeClient;
+import com.sampleproject.departmentservice.controller.DepartmentController;
+import com.sampleproject.departmentservice.model.Department;
+import com.sampleproject.departmentservice.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancedExchangeFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+@Configuration
+public class WebClientConfig {
+
+    @Autowired
+    private LoadBalancedExchangeFilterFunction filterFunction;
+
+
+    @Bean
+    public WebClient employeeWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://employee-service")
+                .filter(filterFunction)
+                .build();
+    }
+
+    @Bean
+    public EmployeeClient employeeClient() {
+        HttpServiceProxyFactory httpServiceProxyFactory
+                = HttpServiceProxyFactory
+                .builder(WebClientAdapter.forClient(employeeWebClient()))
+                .build();
+        return httpServiceProxyFactory.createClient(EmployeeClient.class);
+    }
+}
